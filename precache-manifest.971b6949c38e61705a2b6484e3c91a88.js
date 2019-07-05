b478bb220ddc76b7c1bcd826d66d13c92987bb35
BILLY ENDRES <billyendres@BILLYs-MacBook-Pro.local>
2019-07-05 10:56:39 +1000

    Updates

---
+++ precache-manifest.971b6949c38e61705a2b6484e3c91a88.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "691b42c60116bc19da3e8336109468e3",
+    "url": "/Octivo/static/media/img4.691b42c6.png"
+  },
+  {
+    "revision": "c88d682c1962af0b87803ba95ce2dd93",
+    "url": "/Octivo/static/media/img3.c88d682c.png"
+  },
+  {
+    "revision": "2205b6c3468af8ce33cd466fd2ca51bd",
+    "url": "/Octivo/static/media/img2.2205b6c3.png"
+  },
+  {
+    "revision": "51c4989161ef144c5650b6cfed6161f7",
+    "url": "/Octivo/static/media/img1.51c49891.png"
+  },
+  {
+    "revision": "36e6b693197667fa70d67c989b0aaf6e",
+    "url": "/Octivo/static/media/blog.36e6b693.png"
+  },
+  {
+    "revision": "04509d62e9f4e9afe180",
+    "url": "/Octivo/static/js/runtime~main.04509d62.js"
+  },
+  {
+    "revision": "f50bc53fb1c923af17fc",
+    "url": "/Octivo/static/js/main.f50bc53f.chunk.js"
+  },
+  {
+    "revision": "cdb77520881c264e6ed2",
+    "url": "/Octivo/static/js/1.cdb77520.chunk.js"
+  },
+  {
+    "revision": "f50bc53fb1c923af17fc",
+    "url": "/Octivo/static/css/main.bf801bb1.chunk.css"
+  },
+  {
+    "revision": "8b956b5cda6516920ce7090bf89a1ec7",
+    "url": "/Octivo/index.html"
+  }
+];
